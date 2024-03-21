a8c44e755b05d7e517c6426a0a1e0abff63f9ca2
Ankit <ankit@ankit-3.local>
2024-03-21 11:58:05 +0530

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-import express from 'express';
+const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = new express();
 
-
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
